6b6b17f29274a90628256a17b3cdaefb52509170
xatongxue <1120877019@qq.com>
2020-07-24 10:50:33 +0800

    整合normalize,vant

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 整合normalize
+import 'normalize.css'
+// 导入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 注册vant
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
